40bb39c7c3816155f17ef7e8efcbcb2667c8a67d
RJW <blacksheeprobotics@gmail.com>
2024-02-28 23:38:05 -0500

    Added Output:

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     user_input = input("Input: ")
+    print("Output: ", end='')
     for char in user_input:
         if char.lower() == 'a' or char.lower() == 'e' or char.lower() == 'i' or \
                 char.lower() == 'o' or char.lower() == 'u':
